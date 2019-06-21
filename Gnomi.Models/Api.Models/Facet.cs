8ab0c36bf1e8fb3049db84fe2a040669d7ccb567
Brandon Pauly <brandon.m.pauly@gmail.com>
2019-06-20 21:39:00 -0500

    initial commit; added two models for api

---
+++ Gnomi.Models/Api.Models/Facet.cs
@@ -0,0 +1,18 @@
+﻿namespace Gnomi.Models.Api.Models
+{
+    public class Facet
+    {
+        public Facet(long facetId, string tag, long connections)
+        {
+            FacetId = facetId;
+            Tag = tag;
+            Connections = connections;
+        }
+
+        public long FacetId { get; }
+
+        public string Tag { get; }
+
+        public long Connections { get; }
+    }
+}
---
+++ Gnomi.Models/Api.Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Gnomi.Models.Api.Models
+{
+    public class User
+    {
+        public User(long userId, string firstName, string lastName, DateTime dob, string gender, int zipcode)
+        {
+
+        }
+
+        public long UserId { get; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string Gender { get; set; }
+
+        public int Zipcode { get; set; }
+    }
+}
