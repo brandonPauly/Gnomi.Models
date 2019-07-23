db7c107a5868e25b13353db48966286cce29fbcb
Brandon Pauly <brandon.m.pauly@gmail.com>
2019-07-22 22:36:30 -0500

    moved orchestration request to gnomi.models

---
+++ gnomi.models/orchestration.models/requests/signUpOrchestrationRequest.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace gnomi.orchestration.requests
+{
+    public class signUpOrchestrationRequest
+    {
+        [StringLength(254)]
+        public string email { get; set; }
+
+        [StringLength(43)]
+        public string password { get; set; }
+    }
+}
+
