aa33e93446a984bb7f0f5a870a32bf9db2fc40c8
Brandon Pauly <brandon.m.pauly@gmail.com>
2019-07-31 21:09:19 -0500

    updated some data service models to support querying

--- gnomi.models/dataService.models/entities/human.cs
+++ gnomi.models/dataService.models/entities/human.cs
@@ -1,18 +1,35 @@
 ﻿using gnomi.dataService.metadata;
+using System;
 
 namespace gnomi.dataService.entities
 { 
     public class human<t> : iEntity<t>
     {
+        private iMetadata<t> _metadata;
         public human(iHumanMetadata<t> metadata)
         {
-            this.metadata = metadata;
+            _metadata = metadata;
         }
 
         public string email { get; set; }
 
         public string password { get; set; }
 
-        public iMetadata<t> metadata { get; }
+        public string firstName { get; set; }
+
+        public string lastName { get; set; }
+
+        public DateTime? dateOfBirth { get; set; }
+
+        public string zipcode { get; set; }
+
+        public byte? genderId { get; set; } 
+
+        public iMetadata<t> metadata {
+            get {
+                _metadata.refresh(this);
+                return _metadata;
+            }
+        }
     }
 }
