9cd5f76c33cd9fce7127251c1fa991be5dbb264a
Brandon Pauly <brandon.m.pauly@gmail.com>
2019-09-11 22:44:10 -0500

    moved to and from classes to mailMessage

--- gnomi.models/common.models/smtp/mailMessage.cs
+++ gnomi.models/common.models/smtp/mailMessage.cs
@@ -12,4 +12,18 @@ namespace gnomi.common.models.smtp
 
         public from from { get; set; }
     }
+
+    public class to
+    {
+        public string name { get; set; }
+
+        public string address { get; set; }
+    }
+
+    public class from
+    {
+        public string name { get; set; }
+
+        public string address { get; set; }
+    }
 }
