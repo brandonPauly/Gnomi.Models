9cd08d9fdba6c80b7ba7d461db84a0fb1db9d214
Brandon Pauly <brandon.m.pauly@gmail.com>
2019-09-26 01:09:00 -0500

    added model for orchestration signup response

---
+++ gnomi.models/orchestration.models/response/signUpResponse.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace gnomi.models.orchestration.models.response
+{
+    public class signUpResponse
+    {
+        public long humanId { get; set; }
+
+        public string email { get; set; }
+
+        public string verificationCode { get; set; }
+    }
+}
