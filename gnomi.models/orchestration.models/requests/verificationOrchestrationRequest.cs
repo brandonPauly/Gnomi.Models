326d3b0b77bf73703631164fe2633ddd84c2743f
Brandon Pauly <brandon.m.pauly@gmail.com>
2019-11-13 22:15:53 -0600

    added verification request for orchestration

---
+++ gnomi.models/orchestration.models/requests/verificationOrchestrationRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace gnomi.models.orchestration.models.requests
+{
+    public class verificationOrchestrationRequest
+    {
+        [StringLength(96)]
+        public string verificationCode { get; set; }
+    }
+}
