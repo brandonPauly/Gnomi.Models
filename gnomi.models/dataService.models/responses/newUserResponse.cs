9208d900f5d820ea96d000917967fd101981fdb7
Brandon Pauly <brandon.m.pauly@gmail.com>
2019-11-13 22:06:18 -0600

    added verification code to newUserResponse

--- gnomi.models/dataService.models/responses/newUserResponse.cs
+++ gnomi.models/dataService.models/responses/newUserResponse.cs
@@ -4,8 +4,6 @@ namespace gnomi.dataService.responses
 {
     public class newUserResponse
     {
-        public long humanId { get; set; }
-
         public string email { get; set; }
 
         public string verificationCode { get; set; }
