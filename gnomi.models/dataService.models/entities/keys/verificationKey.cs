18a08c4ee491fd8645fa1601277ab11cac552f66
Brandon Pauly <brandon.m.pauly@gmail.com>
2019-11-16 01:46:15 -0600

    added verification entities for verification repo

---
+++ gnomi.models/dataService.models/entities/keys/verificationKey.cs
@@ -0,0 +1,10 @@
+﻿
+namespace gnomi.dataService.entities.keys
+{
+    public class verificationKey
+    {
+        public string verificationCode { get; set; }
+
+        public long humanId { get; set; }
+    }
+}
