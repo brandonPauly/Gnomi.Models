82c9a07e6bc982b3c0aaee88bfc1306bcf2555b9
Brandon Pauly <brandon.m.pauly@gmail.com>
2019-11-24 01:48:38 -0600

    added mailMessageData class

---
+++ gnomi.models/common.models/smtp/mailMessageData.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace gnomi.common.models.smtp
+{
+    public class mailMessageData
+    {
+        public string toEmailAddress { get; set; }
+
+        public string fromEmailAddress { get; set; }
+
+        public string fromName { get; set; }
+
+        public string body { get; set; }
+
+        public string subject { get; set; }
+    }
+}
